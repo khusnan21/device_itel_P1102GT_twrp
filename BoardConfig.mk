dc6df904fa691f6955b9eb11bcbb4f67d869dd82
khusnan21 <khusnan21@gmail.com>
2025-08-26 00:27:02 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,46 +1,18 @@
-#
-# Copyright (C) 2022 The TWRP Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
+# Device Path
 DEVICE_PATH := device/itel/P1102GT
 
-# Building with minimal manifest
+# Build System
 ALLOW_MISSING_DEPENDENCIES := true
-
-# Build Hack
 BUILD_BROKEN_DUP_RULES := true
 
-# A/B
+# A/B OTA
 AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-	boot \
-	dtbo \
-	system \
-	product \
-	vendor \
-	vbmeta \
-	vendor_boot \
-	vbmeta_system \
-	vbmeta_vendor
+AB_OTA_PARTITIONS += boot dtbo system product vendor vbmeta vendor_boot vbmeta_system vbmeta_vendor
 
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a78
 
@@ -48,13 +20,13 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
 
-# Assertation
+# Assert
 TARGET_OTA_ASSERT_DEVICE := P1102GT
 
 # Bootloader
@@ -62,6 +34,7 @@ TARGET_BOOTLOADER_BOARD_NAME := P1102GT
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_VENDOR_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_PAGE_SIZE := 4096
 BOARD_KERNEL_BASE := 0x3fff8000
@@ -69,20 +42,16 @@ BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x26f08000
 BOARD_TAGS_OFFSET := 0x07c88000
 BOARD_BOOT_HEADER_VERSION := 4
-BOARD_DTB_SIZE := 183032
 BOARD_DTB_OFFSET := 0x07c88000
-BOARD_HEADER_SIZE := 2128
-
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-
 TARGET_NO_KERNEL := false
-BOARD_RAMDISK_USE_LZ4 := true
-BOARD_KERNEL_SEPARATED_DTBO := true
 
+# DTB
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(BOARD_VENDOR_CMDLINE)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE) --board ""
@@ -97,17 +66,16 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
 
-BOARD_SUPER_PARTITION_SIZE := 9126805504
-BOARD_SUPER_PARTITION_GROUPS := alldocube_dynamic_partitions
-BOARD_ALLDOCUBE_DYNAMIC_PARTITIONS_PARTITION_LIST := system product vendor
-BOARD_ALLDOCUBE_DYNAMIC_PARTITIONS_SIZE := 9122611200 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB
+BOARD_SUPER_PARTITION_SIZE := 9663676416
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_PARTITION_LIST := system product vendor
+BOARD_MAIN_SIZE := 9661579264
 
-BOARD_PARTITION_LIST := $(call to-upper, $(BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST))
-$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs))
-$(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
+$(foreach p, $(BOARD_MAIN_PARTITION_LIST), \
+  $(eval BOARD_$(call to-upper,$(p))IMAGE_FILE_SYSTEM_TYPE := erofs) \
+  $(eval TARGET_COPY_OUT_$(call to-upper,$(p)) := $(p)))
 
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
@@ -116,66 +84,52 @@ BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_RECOVERY_KEY_PATH := (DEVICE_PATH)/external/test.pem
+BOARD_AVB_RECOVERY_KEY_PATH := $(DEVICE_PATH)/external/testkey.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
-# Resolution
-TARGET_SCREEN_HEIGHT := 2000
-TARGET_SCREEN_WIDTH := 1200
-
-# Treble
-BOARD_VNDK_VERSION := current
-TARGET_COPY_OUT_VENDOR := vendor
+# Display & Theme
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
+TW_FRAMERATE := 60
+TW_THEME := portrait_hdpi
+TW_DEFAULT_BRIGHTNESS := 1000
 
 # Recovery
-TARGET_NO_RECOVERY := true
-#BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-
-# vendor_boot recovery ramdisk
-#BOARD_USES_RECOVERY_AS_BOOT := 
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := 
-BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := 
+BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 
-# Debug
-TARGET_USES_LOGD := true
-TWRP_INCLUDE_LOGCAT := true
-
-# Crypto
+# Crypto & Encryption
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
 BOARD_USES_METADATA_PARTITION := true
 
-# Encryption
 PLATFORM_SECURITY_PATCH := 2127-12-31
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 PLATFORM_VERSION := 127
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
-# Tools
+# Debug & Tools
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
 TW_INCLUDE_RESETPROP := true
-TW_INCLUDE_LIBRESETPROP :=true
+TW_INCLUDE_LIBRESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 
 # USB
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
-
-# Display
-TW_FRAMERATE := 60
-TW_THEME := portrait_hdpi
-TW_DEFAULT_BRIGHTNESS := 1000
 
 # Temp
 TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone54/temp
@@ -183,13 +137,10 @@ TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone54/temp
 # Fastbootd
 TW_INCLUDE_FASTBOOTD := true
 
-# Language
+# Language & Misc
 TW_EXTRA_LANGUAGES := true
-
-# Other TWRP Configurations
 TW_EXCLUDE_APEX := true
 TW_INCLUDE_NTFS_3G := true
 TARGET_USES_MKE2FS := true
 TW_INCLUDE_FUSE_EXFAT := true
 TW_INPUT_BLACKLIST := hbtp_vm
-
