0f91c9ce53ebb63fbb61f97f46457c39b8453819
khusnan21 <khusnan21@gmail.com>
2025-09-05 02:38:43 +0700

    Push semua file device tree

---
+++ twrp_P1102GT.mk
@@ -0,0 +1,42 @@
+
+$(call inherit-product, device/itel/P1102GT/patches.mk ) 
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Configure twrp config common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device specific configs
+$(call inherit-product, device/itel/P1102GT/device.mk)
+
+# Configure launch_with_vendor_ramdisk.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Configure virtual_ab_ota.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+PRODUCT_DEVICE := P1102GT
+PRODUCT_NAME := twrp_P1102GT
+PRODUCT_BRAND := itel
+PRODUCT_MODEL := P1102GT
+PRODUCT_MANUFACTURER := itel
+PRODUCT_RELEASE_NAME := itel P1102GT
+
+PRODUCT_BOARD := P1102GT
+
+# Product Specifics
+PRODUCT_NAME := twrp_P1102GT
+PRODUCT_DEVICE := P1102GT
+PRODUCT_BRAND := itel 
+PRODUCT_MODEL := itel P1102GT
+PRODUCT_MANUFACTURER := itel
+
+PRODUCT_GMS_CLIENTID_BASE := android-itel
