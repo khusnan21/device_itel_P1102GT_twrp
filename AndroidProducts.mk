111f333b9c9ce44bfbf0e75d79cbfbbb9eeb4d0f
khusnan21 <khusnan21@gmail.com>
2025-09-05 18:42:00 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,11 @@
-PRODUCT_DEVICE := $(notdir $(LOCAL_DIR))
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_$(PRODUCT_DEVICE).mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_P1102GT.mk
 
-COMMON_LUNCH_CHOICES := twrp_$(PRODUCT_DEVICE)-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_P1102GT-eng
